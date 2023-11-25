d08217633c51da88c7bc1cfc72646c3b7b5afcc8
Manikanta M <manikanta.m@labsquire.com>
2023-11-25 09:31:29 +0530

    Select filter implemented

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const apiURL = "https://api.thecatapi.com/v1/images/search?limit=10";
+const apiURL = "https://api.thecatapi.com/v1";
+let imagesLimit = 5;
+let randomImages = `/images/search?limit=${imagesLimit}`;
+const allbreeds = "/breeds";
 const apiKey =
   "live_w7GAshIqDq7ygWT6Qr2GoAYlijzudgFU6swXLqsmt3WyUGa6yt1QhA6IruE7KuVn";
 let loadingElement = document.getElementById("loading");
 let isLoading = true;
-
-async function getData() {
+let getSelectedImages = document.getElementById("selectImageFilter");
+console.log(randomImages);
+async function getData(apiSlug) {
   try {
-    const response = await fetch(apiURL, { headers: { "x-api-key": apiKey } });
+    const response = await fetch(apiURL + apiSlug, {
+      headers: { "x-api-key": apiKey },
+    });
     if (response.ok) {
       setLoading(false);
       const data = await response.json();
@@ -23,8 +29,16 @@ function setLoading(isLoading) {
   loadingElement.style.display = isLoading ? "block" : "none";
 }
 
+function selectedImagesFilter() {
+  console.log(getSelectedImages.value);
+  this.imagesLimit = getSelectedImages.value;
+  getData(randomImages);
+  let filterCountDisplay = document.getElementById("filterCount");
+  filterCountDisplay.innerText = getSelectedImages.value + " images selected";
+}
+
 function displayData(data) {
-  const displayElement = document.querySelector("#app ul");
+  const displayElement = document.querySelector("#randomImages");
   data.forEach((cat) => {
     const li = document.createElement("li");
     const img = document.createElement("img");
@@ -38,4 +52,4 @@ function displayData(data) {
 }
 
 // calling this function
-getData();
+selectedImagesFilter();
